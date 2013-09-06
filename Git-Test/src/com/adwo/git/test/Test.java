12db86fc08a723283b0589e5fe1c97531d5662b5
Han Wang <cswanghan@gmail.com>
2013-09-06 15:47:40 +0800

    Added Test.java

---
+++ Git-Test/src/com/adwo/git/test/Test.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.adwo.git.test;
+
+/**
+ * @author Han Wang
+ *
+ */
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
